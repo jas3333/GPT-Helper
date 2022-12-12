df49f8bcff0019ec8ae51aa41165963d4364798d
jas3333 <accounts1@protonmail.com>
2022-12-11 16:10:13 -0800

    Added textarea instead of input. Can shift enter to add more lines

--- client/src/components/Prompt.js
+++ client/src/components/Prompt.js
@@ -1,18 +1,39 @@
+import { useState } from 'react';
+
 const Prompt = ({ question, setQuestion, onSubmit, loading }) => {
+    const [rows, setRows] = useState(1);
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && event.shiftKey && rows !== 5) {
+            setRows(rows + 1);
+        }
+    };
+
+    const handleSubmit = (event) => {
+        if (event.keyCode === 13 && !event.shiftKey) {
+            event.preventDefault();
+            setRows(1);
+            onSubmit(event);
+        }
+    };
     return (
         <div className='container auto content-center align mg-bot-lg '>
-            <form className='size-form-special ' onSubmit={onSubmit}>
-                <input
-                    type='text'
-                    name=''
-                    value={question}
-                    className='input-text-lg full bg-mid'
-                    placeholder='Ask a question'
-                    onChange={(event) => setQuestion(event.target.value)}
-                    autoFocus={true}
-                />
-            </form>
-            <div className='loader '>{loading && <div className='spinner'></div>}</div>
+            <div className='container '>
+                <form onKeyDown={handleKeyDown}>
+                    <textarea
+                        rows={rows}
+                        resize='none'
+                        value={question}
+                        onChange={(event) => setQuestion(event.target.value)}
+                        cols='50'
+                        className='input-text '
+                        onKeyDown={handleSubmit}
+                        autoFocus={true}
+                        placeholder='Ask a question'
+                    />
+                </form>
+                <div className='loader '>{loading && <div className='spinner '></div>}</div>
+            </div>
         </div>
     );
 };
